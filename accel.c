7e9fd936c122f923b611e25f1a82458a02189188
Bryan Blakeslee <bblakeslee4@gmail.com>
2017-04-05 18:30:16 -0400

    Finished read function.

--- accel.c
+++ accel.c
@@ -15,16 +15,43 @@ void init_accel(void){
 	// spi_write(A_BOOT);
 	
 	// Configure full scale sensing at +-4g
+	select_accel();
 	spi_write(A_WRITE | CTRL_REG4_A);
 	spi_write(A_FS_1);
+	deselect_accel();
 	
 	// Configure block data update, output data rate (10Hz), and enable axes
+	select_accel();
 	spi_write(A_WRITE | CTRL_REG1_A);
 	spi_write(A_BDU | A_X_EN | A_Y_EN | A_Z_EN | A_ODR_0);
+	deselect_accel();
 }
 
 uint16_t read_accel_axis(accel_axis axis){
-	spi_write(A_READ | 
+	uint8_t addr_low, addr_high;
+	uint16_t data_low, data_high;
+	
+	// Address decode
+	if(axis == X_AXIS){
+		addr_low = A_OUT_X_L;
+		addr_high = A_OUT_X_H;
+	}else if(axis == Y_AXIS){
+		addr_low = A_OUT_Y_L;
+		addr_high = A_OUT_Y_H;
+	}else if(axis == Z_AXIS){
+		addr_low = A_OUT_Z_L;
+		addr_high = A_OUT_Z_H;
+	}
+	
+	// Get low data byte
+	spi_write(A_READ | addr_low);
+	data_low = spi_read();
+	
+	// Get high data byte
+	spi_write(A_READ | addr_high);
+	data_high = spi_read();
+	
+	return ((data_high << 8) | data_low);
 }
 
 void select_accel(void){
