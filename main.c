66fc1f1e9b049f3f63ea998f6b24794732e30d90
Bryan Blakeslee <bblakeslee4@gmail.com>
2017-04-05 18:29:34 -0400

    Wrote test code.

--- main.c
+++ main.c
@@ -2,15 +2,18 @@
 #include "main.h"
 
 int main(void){
-	uint32_t i;
+	uint16_t data_x = 0;
+	uint16_t data_y = 0;
+	uint16_t data_z = 0;
 	// Hardware configuration
 	System_Clock_Init();
+	UART2_Init();
 	init_spi();
 	init_accel();
 	
-	while(1){
-		select_accel();
-		spi_write('A');
-		deselect_accel();
-	}
+	USART_Read(USART2);
+	data_x = read_accel_axis(X_AXIS);
+	data_y = read_accel_axis(Y_AXIS);
+	data_z = read_accel_axis(Z_AXIS);
+	while(1);
 }
