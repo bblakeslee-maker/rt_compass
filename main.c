fc71cf9e37e644393a35dff412a19762ef3e239d
Bryan Blakeslee <bblakeslee4@gmail.com>
2017-04-23 14:41:29 -0400

    Started flow controlled compass polling.

--- main.c
+++ main.c
@@ -2,13 +2,7 @@
 #include "main.h"
 
 int main(void){
-	/* uint16_t data_x = 0;
-	uint16_t data_y = 0;
-	uint16_t data_z = 0;
-	uint8_t reg; */
-	
-	int i;
-	uint8_t test_data = 'B';
+	uint8_t status;	// Holder for status register info
 	
 	// Hardware configuration
 	System_Clock_Init();
@@ -16,26 +10,13 @@ int main(void){
 	init_spi();
 	init_accel();
 	
-	// Read test code
-	select_accel();
-	spi_write(A_WRITE | CTRL_REG3_A);
-	spi_write((uint8_t)'A');
-	deselect_accel();
-	
-	for(i = 0; i < 100; i++);
-	
-	select_accel();
-	spi_write(A_READ | CTRL_REG3_A);
-	test_data = spi_read();
-	deselect_accel();
-	
-	select_accel();
-	spi_write(0xDE);
-	deselect_accel();
-	
-	USART_Write(USART2, (uint8_t*)"Test: ", strlen("Test: "));
-	USART_Write(USART2, &test_data, 1);
-	USART_Write(USART2, (uint8_t*)"\n\r", 2);
-	
-	while(1);
+	while(1){
+		while(!(GPIOE->IDR & ACCEL_DATA_READY_FLAG));
+		status = check_accel_status();
+		if(status & ZYXDA){
+			read_accel_axis(X_AXIS);
+			read_accel_axis(Y_AXIS);
+			read_accel_axis(Z_AXIS);
+		}
+	}
 }
