7e3d481625b7a4924b2e6e024eedf7b5712e68cd
Bryan Blakeslee <bblakeslee4@gmail.com>
2017-04-25 12:24:49 -0400

    Added roll/pitch code.  Check this for 1st quadrant problem.

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 // Include files
 #include "main.h"
-#include <stdio.h>
+
+const float PI = 3.14159265358979323846;
+const float RADIAN_TO_DEGREE = 180 / PI;
 
 int main(void){
 	uint8_t status;											// Holder for status register info
 	uint16_t count_x, count_y, count_z;	// Gravity levels in counts
-	float mg_x, mg_y, mg_z;								// Gravity levels in mG's
+	float mg_x, mg_y, mg_z;							// Gravity levels in mG's
+	float roll_rad, pitch_rad;					// Roll and pitch in radians
+	float roll_deg, pitch_deg;					// Roll and pitch in degrees
+	float mg_z2;												// Intermediary pitch variable
 	
-	char prompt[20];
-	char buffer_x[20];
-	char buffer_y[20];
-	char buffer_z[20];
+	// Debug
 	int i;
+	char buffer[40];
 	
 	// Hardware configuration
 	System_Clock_Init();
@@ -19,7 +22,7 @@ int main(void){
 	init_spi();
 	init_accel();
 	
-	for(i = 0; i < 10; i++){
+	for(i = 0; i < 10; i++){//while(1){
 		while(!(GPIOE->IDR & ACCEL_DATA_READY_FLAG));
 		status = check_accel_status();
 		if(status & ZYXDA){
@@ -32,20 +35,17 @@ int main(void){
 			mg_x = (float)count_x * COUNT_TO_MG;
 			mg_y = (float)count_y * COUNT_TO_MG;
 			mg_z = (float)count_z * COUNT_TO_MG;
-			sprintf(prompt, "i = %d\n\r", i);
-			sprintf(buffer_x, "%f", mg_x);
-			sprintf(buffer_y, "%f", mg_y);
-			sprintf(buffer_z, "%f", mg_z);
-			USART_Write(USART2, (uint8_t*)prompt, strlen(prompt));
-			USART_Write(USART2, (uint8_t*)"mg_x = ", strlen("mg_x = "));
-			USART_Write(USART2, (uint8_t*)buffer_x, strlen(buffer_x));
-			USART_Write(USART2, (uint8_t*)"\n\r", strlen("\n\r"));
-			USART_Write(USART2, (uint8_t*)"mg_y = ", strlen("mg_y = "));
-			USART_Write(USART2, (uint8_t*)buffer_y, strlen(buffer_y));
-			USART_Write(USART2, (uint8_t*)"\n\r", strlen("\n\r"));
-			USART_Write(USART2, (uint8_t*)"mg_z = ", strlen("mg_z = "));
-			USART_Write(USART2, (uint8_t*)buffer_z, strlen(buffer_z));
-			USART_Write(USART2, (uint8_t*)"\n\r", strlen("\n\r"));
+			
+			roll_rad = atan2(mg_y, mg_z);									// Compute roll
+			mg_z2 = mg_y * sin(roll_rad) + mg_z * cos(roll_rad);	// Pitch intermediary
+			pitch_rad = atan2(-mg_x, mg_z2);							// Compute pitch
+			
+			roll_deg = roll_rad * RADIAN_TO_DEGREE;				// Convert to degree
+			sprintf(buffer, "Roll = %f\n\r", roll_deg);
+			USART_Write(USART2, (uint8_t*)buffer, strlen(buffer));
+			pitch_deg = pitch_rad * RADIAN_TO_DEGREE;			// Convert to degree
+			sprintf(buffer, "Pitch = %f\n\r", pitch_deg);
+			USART_Write(USART2, (uint8_t*)buffer, strlen(buffer));
 		}
 	}
 }
--- main.h
+++ main.h
@@ -4,9 +4,17 @@
 // Include files
 #include "stm32l476xx.h"
 #include "SysClock.h"
+#include <math.h>
+#include <stdio.h>
 #include "UART.h"
 #include "string.h"
 #include "spi.h"
 #include "accel.h"
+
+// Define pi (can't find it in math.h)
+extern const float PI;
+
+// Radian to degree scale factor
+extern const float RADIAN_TO_DEGREE;
 
 #endif
