aeb6d995045e1ae06681e1df1f0e46a3f63ba581
Bryan Blakeslee <bblakeslee4@gmail.com>
2017-05-09 15:03:48 -0400

    First draft of compass application.  Need to test.

--- main.c
+++ main.c
@@ -6,8 +6,8 @@ int main(void){
 	int16_t count_x, count_y, count_z;		// Gravity levels in counts
 	double mGauss_x, mGauss_y, mGauss_z;	// Magnetic levels in milligauss
 	double yaw_rad;												// Yaw angle in radians
-	//double mg_x, mg_y, mg_z;						// Gravity levels in mG's
-	//double roll_rad, pitch_rad;					// Roll and pitch in radians
+	double mg_x, mg_y, mg_z;							// Gravity levels in mG's
+	double roll_rad, pitch_rad;						// Roll and pitch in radians
 	
 #ifdef DEBUG
 	int i;
@@ -21,18 +21,44 @@ int main(void){
 	init_mag();
 	
 	for(i = 0; i < 10; i++){
+		while(!accel_data_ready());
 		while(!mag_data_ready());
+		
+		status = check_accel_status();
+		if(status & ZYXDA_A){
+			// Get accel data if data is ready on all three axes
+			count_x = read_accel_axis(X_AXIS_A);
+			count_y = read_accel_axis(Y_AXIS_A);
+			count_z = read_accel_axis(Z_AXIS_A);
+			
+			// Convert to milliG
+			mg_x = count_to_mg(count_x);
+			mg_y = count_to_mg(count_y);
+			mg_z = count_to_mg(count_z);
+
+			// Compute tilt angles
+			roll_rad = compute_roll(mg_y, mg_z);
+			pitch_rad = compute_pitch(roll_rad, mg_x, mg_y, mg_z);
+			
+#ifdef DEBUG
+			print_accel_telemetry(mg_x, mg_y, mg_z, roll_rad, pitch_rad);
+#endif
+		}
+		
 		status = check_mag_status();
 		if(status & ZYXDA_M){
+			// Get mag data if data is ready on all three axes
 			count_x = read_mag_axis(X_AXIS_M);
 			count_y = read_mag_axis(Y_AXIS_M);
 			count_z = read_mag_axis(Z_AXIS_M);
 			
+			// Convert to milligauss
 			mGauss_x = count_to_milligauss(count_x);
 			mGauss_y = count_to_milligauss(count_y);
 			mGauss_z = count_to_milligauss(count_z);
 			
-			//yaw_rad = compute_yaw(roll_rad, pitch_rad, mGauss_x, mGauss_y, mGauss_z);
+			// Compute compass heading
+			yaw_rad = compute_yaw(roll_rad, pitch_rad, mGauss_x, mGauss_y, mGauss_z);
 			
 #ifdef DEBUG
 			print_mag_telemetry(mGauss_x, mGauss_y, mGauss_z, yaw_rad);
@@ -42,33 +68,4 @@ int main(void){
 #ifdef DEBUG
 	USART_Write(USART2, (uint8_t*)"**********\n\r", strlen("**********\n\r"));
 #endif
-	
-/*
-#ifdef DEBUG
-	for(i = 0; i < 10; i++){
-#else
-	while(1){
-#endif
-		while(!accel_data_ready());
-		status = check_accel_status();
-		if(status & ZYXDA_A){
-			// Get data if data is ready on all three axes
-			count_x = read_accel_axis(X_AXIS_A);
-			count_y = read_accel_axis(Y_AXIS_A);
-			count_z = read_accel_axis(Z_AXIS_A);
-			
-			mg_x = count_to_mg(count_x);
-			mg_y = count_to_mg(count_y);
-			mg_z = count_to_mg(count_z);
-
-			roll_rad = compute_roll(mg_y, mg_z);
-			pitch_rad = compute_pitch(roll_rad, mg_x, mg_y, mg_z);
-			
-#ifdef DEBUG
-			print_accel_telemetry(mg_x, mg_y, mg_z, roll_rad, pitch_rad);
-			USART_Write(USART2, (uint8_t*)"**********\n\r", strlen("**********\n\r"));
-#endif
-		}
-	}
-	*/
 }
