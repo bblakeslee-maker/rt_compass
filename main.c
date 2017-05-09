0d564a3c4edd06b8477e45c8a18aa2159943550e
Bryan Blakeslee <bblakeslee4@gmail.com>
2017-05-09 14:56:51 -0400

    Added yaw test code.

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include "main.h"
 
 int main(void){
-	uint8_t status;											// Holder for status register info
-	int16_t count_x, count_y, count_z;	// Gravity levels in counts
-	double mg_x, mg_y, mg_z;						// Gravity levels in mG's
-	double roll_rad, pitch_rad;					// Roll and pitch in radians
+	uint8_t status;												// Holder for status register info
+	int16_t count_x, count_y, count_z;		// Gravity levels in counts
+	double mGauss_x, mGauss_y, mGauss_z;	// Magnetic levels in milligauss
+	double yaw_rad;												// Yaw angle in radians
+	//double mg_x, mg_y, mg_z;						// Gravity levels in mG's
+	//double roll_rad, pitch_rad;					// Roll and pitch in radians
 	
 #ifdef DEBUG
 	int i;
@@ -18,12 +20,36 @@ int main(void){
 	init_accel();
 	init_mag();
 	
+	for(i = 0; i < 10; i++){
+		while(!mag_data_ready());
+		status = check_mag_status();
+		if(status & ZYXDA_M){
+			count_x = read_mag_axis(X_AXIS_M);
+			count_y = read_mag_axis(Y_AXIS_M);
+			count_z = read_mag_axis(Z_AXIS_M);
+			
+			mGauss_x = count_to_milligauss(count_x);
+			mGauss_y = count_to_milligauss(count_y);
+			mGauss_z = count_to_milligauss(count_z);
+			
+			//yaw_rad = compute_yaw(roll_rad, pitch_rad, mGauss_x, mGauss_y, mGauss_z);
+			
+#ifdef DEBUG
+			print_mag_telemetry(mGauss_x, mGauss_y, mGauss_z, yaw_rad);
+#endif
+		}
+	}
+#ifdef DEBUG
+	USART_Write(USART2, (uint8_t*)"**********\n\r", strlen("**********\n\r"));
+#endif
+	
+/*
 #ifdef DEBUG
 	for(i = 0; i < 10; i++){
 #else
 	while(1){
 #endif
-		while(!(GPIOE->IDR & ACCEL_DATA_READY_FLAG));
+		while(!accel_data_ready());
 		status = check_accel_status();
 		if(status & ZYXDA_A){
 			// Get data if data is ready on all three axes
@@ -40,7 +66,9 @@ int main(void){
 			
 #ifdef DEBUG
 			print_accel_telemetry(mg_x, mg_y, mg_z, roll_rad, pitch_rad);
+			USART_Write(USART2, (uint8_t*)"**********\n\r", strlen("**********\n\r"));
 #endif
 		}
 	}
+	*/
 }
